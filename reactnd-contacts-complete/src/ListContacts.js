a3962972581f03696b8e5981a6007904182632f5
RoseKimball <mrkimbal@usc.edu>
2020-12-01 10:59:48 -0800

    init

---
+++ reactnd-contacts-complete/src/ListContacts.js
@@ -0,0 +1,25 @@
+import React, { Component } from "react";
+
+class ListContacts extends Component {
+  render() {
+    return (
+      <ol className="contact-list">
+        {this.props.contacts.map(contact => (
+          <li className="contact-list-item" key={contact.id}>
+            <div
+              className="contact-avatar"
+              style={{ backgroundImage: `url(${contact.avatarURL})` }}
+            ></div>
+            <div className="contact-details">
+              <p>{contact.name}</p>
+              <p>{contact.details}</p>
+            </div>
+            <button className="contact-remove">remove</button>
+          </li>
+        ))}
+      </ol>
+    );
+  }
+}
+
+export default ListContacts;
